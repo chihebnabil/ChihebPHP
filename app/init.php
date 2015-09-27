1d342efa57bd334f5a48ee248934d970022b192b
cnabil <chiheb.design@gmail.com>
2015-09-27 17:30:02 +0100

    helpers and root

--- app/init.php
+++ app/init.php
@@ -2,12 +2,18 @@
 define('ROOT',dirname(__DIR__));
 
 
+define('APP',ROOT."/app/");
+
+
+define('CSS',ROOT."/public/css/");
+
+
 require_once ROOT.'/vendor/autoload.php';
-require_once ROOT.'/app/core/App.php';
-require_once ROOT.'/app/database.php';
-require_once ROOT.'/app/core/components/Auth.php';
-require_once ROOT.'/app/core/helpers/html.php';
-require_once ROOT."/app/core/Controller.php";
+require_once APP.'/core/App.php';
+require_once APP.'/database.php';
+require_once APP.'/core/components/Auth.php';
+require_once APP.'/core/helpers/html.php';
+require_once APP."/core/Controller.php";
 
 
 
